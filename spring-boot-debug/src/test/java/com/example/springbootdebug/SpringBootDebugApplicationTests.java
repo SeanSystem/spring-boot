4a37d962702ca9d7506ac59f06ae13fa6fabd2f7
huyb <huyb@smartebao.com>
2022-09-13 16:33:12 +0800

    添加debug模块

---
+++ spring-boot-debug/src/test/java/com/example/springbootdebug/SpringBootDebugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDebugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
